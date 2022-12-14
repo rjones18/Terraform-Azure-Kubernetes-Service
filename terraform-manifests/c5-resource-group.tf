a529e4399d346474a170bf82bc26f02b581c33ce
Reginald Jones <reggiej3939@gmail.com>
2022-12-13 22:37:30 -0500

    Updating rg

--- terraform-manifests/c5-resource-group.tf
+++ terraform-manifests/c5-resource-group.tf
@@ -1,5 +1,5 @@
 # Resource-1: Azure Resource Group.
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "test-grp" {
   # name = "${local.resource_name_prefix}-${var.resource_group_name}"
   name = "${local.resource_name_prefix}-${var.resource_group_name}-${random_string.myrandom.id}"
   location = var.resource_group_location
