3602def61aa3b1d4f60804578361ee2f0054873f
Reginald Jones <reggiej3939@gmail.com>
2022-12-15 22:15:51 -0500

    Add storage code for tfstate

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -17,6 +17,14 @@ terraform {
   }
 }
 
+# Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "test-grp"
+    storage_account_name  = "regterraformstate201"
+    container_name        = "tfstatefiles"
+    key                   = "dev-terraform.tfstate"
+  }  
+
 # Provider Block
 provider "azurerm" {
  features {}          
